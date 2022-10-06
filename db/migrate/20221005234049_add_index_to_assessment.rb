9a64a40fb66944adf18bb9e3ac998bb6aa8d68ef
William James Moorcraft <william.moorcraft@student.adelaide.edu.au>
2022-10-06 10:36:33 +1030

    API calls!

---
+++ db/migrate/20221005234049_add_index_to_assessment.rb
@@ -0,0 +1,5 @@
+class AddIndexToAssessment < ActiveRecord::Migration[7.0]
+  def change
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_053834) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_234049) do
   create_table "assessments", force: :cascade do |t|
     t.string "name"
     t.integer "max_score"
