687b2d2f4309caacc7f1a427b8f63fcdd7127e33
Joshua Burge <joshua.burge@student.adelaide.edu.au>
2022-10-07 11:01:25 +1030

    Now have canvas access tokens in the branch, trying to assign teachers from canvas to courses through api now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,92 +15,95 @@ class ApplicationController < ActionController::Base
     puts "Running API calls!"
     # check if student or teacher is logged in
     puts "#{current_user}"
-    if current_user :student and (current_user.last_canvas_request == nil or ((DateTime.now - current_user.last_canvas_request) * 24 * 60).to_i  > 19)
-      puts "student"
-      #---do the API calls---
-      # retrieve data form access toke, as of branch (json_to_database_converter) access_token not implemented
-      # set the response body to nil
-      res = nil
-      # will change this to access_token = current_user.access_token
-      # Chris' access token
-      #access_token = "7036~1Zqk4k0rh1nxLirHTdH8Vbrw55twnvPp0MNBh2954EtITlQAU80JQPeniKXFK7tm"
-      #---
-      access_token = current_user.access_token
-      #---
-      res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses?per_page=50", {:Authorization => "Bearer #{access_token}"}
-      puts "courses request sent"
-      data = JSON.parse(res.body)
-      # gets the date of today
-      now = Date.today
-      current_user.last_canvas_request = DateTime.now
-      # sets the time period to a year before
-      period = (now - 365)
-      # enrollment term id
-      eti = 0
-      courses = []
-      # gets the most recent enrollment term id
-      data.each do |d|
-        if d["created_at"] == nil
-          # if created at is nil, skip as it throws an error
-        elsif DateTime.parse(d["created_at"]).to_date > period
-          if d["enrollment_term_id"] >= eti
-            eti = d["enrollment_term_id"]
-            courses << d
+
+    if student_signed_in?
+      if current_user.last_canvas_request == nil or ((DateTime.now - current_user.last_canvas_request) * 24 * 60).to_i  > 19
+        puts "student"
+        #---do the API calls---
+        # retrieve data form access toke, as of branch (json_to_database_converter) access_token not implemented
+        # set the response body to nil
+        res = nil
+        # will change this to access_token = current_user.access_token
+        # Chris' access token
+        #access_token = "7036~1Zqk4k0rh1nxLirHTdH8Vbrw55twnvPp0MNBh2954EtITlQAU80JQPeniKXFK7tm"
+        #---
+        access_token = current_user.access_token
+        #---
+        res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses?per_page=50", {:Authorization => "Bearer #{access_token}"}
+        puts "courses request sent"
+        data = JSON.parse(res.body)
+        # gets the date of today
+        now = Date.today
+        current_user.last_canvas_request = DateTime.now
+        # sets the time period to a year before
+        period = (now - 365)
+        # enrollment term id
+        eti = 0
+        courses = []
+        # gets the most recent enrollment term id
+        data.each do |d|
+          if d["created_at"] == nil
+            # if created at is nil, skip as it throws an error
+          elsif DateTime.parse(d["created_at"]).to_date > period
+            if d["enrollment_term_id"] >= eti
+              eti = d["enrollment_term_id"]
+              courses << d
+            end
           end
         end
-      end
 
-      courses.to_json
+        courses.to_json
 
-      #for each course in json
-      courses.each do |c|
-        if c["enrollment_term_id"] == eti
-          # Print course name
+        #for each course in json
+        courses.each do |c|
+          if c["enrollment_term_id"] == eti
+            # Print course name
 
-          #check course is in database
-          if not Course.exists?(canvas_id: c["id"])
-            #doesnt exist
-            #puts "Course.new(name: #{c["name"]}, canvas_id: #{c["id"]}, uuid: #{c["uuid"]}), enrollment_term_id: #{c["enrollment_term_id"]}"
-            course = Course.create(name: c["name"], canvas_id: c["id"], uuid: c["uuid"], enrollment_term_id: c["enrollment_term_id"])
-          else
-            puts "Course found: #{c["name"]}"
-            course = Course.find_by(canvas_id: c["id"])
-          end
-
-          # get assignmnets for the course
-          res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/assignments?per_page=40", {:Authorization => "Bearer #{access_token}"}
-          # res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/assignments?per_page=40", {:Authorization => "Bearer #{access_token}"}
-          assignments = JSON.parse(res.body)
-
-          #get submissions
-          res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{c["id"]}/students/submissions?per_page=40", {:Authorization => "Bearer #{access_token}"}
-          # res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/students/submissions?per_page=40", {:Authorization => "Bearer #{access_token}"}
-          submissions = JSON.parse(res.body)
-
-          #for each assignment in json, do work on submission
-          assignments.each_with_index do |a|
-            #check assessment is in database
-            if not Assessment.exists?(canvas_id: a["id"])
+            #check course is in database
+            if not Course.exists?(canvas_id: c["id"])
               #doesnt exist
-              puts "Creating Assessment: Assessment.new(canvas_id: #{a["id"]}, name: #{a["name"]}, max_score: #{a["points_possible"]}, due_date: #{a["due_at"]}, release_date: #{a["unlock_at"]}, canvas_course_id: #{a["course_id"]})"
-              assessment = Assessment.create(course_id: course.id,canvas_id: a["id"], name: a["name"], max_score: a["points_possible"], due_date: a["due_at"], release_date: a["unlock_at"], canvas_course_id: a["course_id"])
+              #puts "Course.new(name: #{c["name"]}, canvas_id: #{c["id"]}, uuid: #{c["uuid"]}), enrollment_term_id: #{c["enrollment_term_id"]}"
+              course = Course.create(name: c["name"], canvas_id: c["id"], uuid: c["uuid"], enrollment_term_id: c["enrollment_term_id"])
             else
-              puts "Found #{a["name"]}"
-              assessment = Assessment.find_by(canvas_id: a["id"])
+              puts "Course found: #{c["name"]}"
+              course = Course.find_by(canvas_id: c["id"])
             end
 
-            # get submission grade
-            # change a["id"] to assessment.canvas_id
-            pos = submissions.find_index {|s| s["assignment_id"] == a["id"]}
-            puts "grade: #{submissions[pos]["score"]}/#{a["points_possible"]}"
-            score = submissions[pos]["score"]
-            if not Grade.exists?(canvas_student_id: submissions[pos]["user_id"] ,canvas_id: submissions[pos]["id"], canvas_assignment_id: a["id"], graded_at: submissions[pos]["graded_at"])
-              # if there is no grade that exists with the same canvas id and graded_at date
-              # create a new grade
-              puts "Grade.new(student: student, assessment_id: assessment.id, score: #{submissions[pos]["score"]}, canvas_student_id: #{submissions[pos]["user_id"]}, canvas_id: #{submissions[pos]["id"]}, canvas_assignment_id: #{submissions[pos]["assignment_id"]}, graded_at: #{submissions[pos]["graded_at"]})"
-              @grade = Grade.create(student_id: current_user.id, assessment_id: assessment.id, score: submissions[pos]["score"], canvas_student_id: submissions[pos]["user_id"], canvas_id: submissions[pos]["id"], canvas_assignment_id: submissions[pos]["assignment_id"], graded_at: submissions[pos]["graded_at"])
-            else
-              puts "Grade already exists and has not been updated"
+            # get assignmnets for the course
+            res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/assignments?per_page=40", {:Authorization => "Bearer #{access_token}"}
+            # res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/assignments?per_page=40", {:Authorization => "Bearer #{access_token}"}
+            assignments = JSON.parse(res.body)
+
+            #get submissions
+            res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{c["id"]}/students/submissions?per_page=40", {:Authorization => "Bearer #{access_token}"}
+            # res = RestClient.get "https://myuni.adelaide.edu.au/api/v1/courses/#{course.canvas_id}/students/submissions?per_page=40", {:Authorization => "Bearer #{access_token}"}
+            submissions = JSON.parse(res.body)
+
+            #for each assignment in json, do work on submission
+            assignments.each_with_index do |a|
+              #check assessment is in database
+              if not Assessment.exists?(canvas_id: a["id"])
+                #doesnt exist
+                puts "Creating Assessment: Assessment.new(canvas_id: #{a["id"]}, name: #{a["name"]}, max_score: #{a["points_possible"]}, due_date: #{a["due_at"]}, release_date: #{a["unlock_at"]}, canvas_course_id: #{a["course_id"]})"
+                assessment = Assessment.create(course_id: course.id,canvas_id: a["id"], name: a["name"], max_score: a["points_possible"], due_date: a["due_at"], release_date: a["unlock_at"], canvas_course_id: a["course_id"])
+              else
+                puts "Found #{a["name"]}"
+                assessment = Assessment.find_by(canvas_id: a["id"])
+              end
+
+              # get submission grade
+              # change a["id"] to assessment.canvas_id
+              pos = submissions.find_index {|s| s["assignment_id"] == a["id"]}
+              puts "grade: #{submissions[pos]["score"]}/#{a["points_possible"]}"
+              score = submissions[pos]["score"]
+              if not Grade.exists?(canvas_student_id: submissions[pos]["user_id"] ,canvas_id: submissions[pos]["id"], canvas_assignment_id: a["id"], graded_at: submissions[pos]["graded_at"])
+                # if there is no grade that exists with the same canvas id and graded_at date
+                # create a new grade
+                puts "Grade.new(student: student, assessment_id: assessment.id, score: #{submissions[pos]["score"]}, canvas_student_id: #{submissions[pos]["user_id"]}, canvas_id: #{submissions[pos]["id"]}, canvas_assignment_id: #{submissions[pos]["assignment_id"]}, graded_at: #{submissions[pos]["graded_at"]})"
+                @grade = Grade.create(student_id: current_user.id, assessment_id: assessment.id, score: submissions[pos]["score"], canvas_student_id: submissions[pos]["user_id"], canvas_id: submissions[pos]["id"], canvas_assignment_id: submissions[pos]["assignment_id"], graded_at: submissions[pos]["graded_at"])
+              else
+                puts "Grade already exists and has not been updated"
+              end
             end
           end
         end
--- db/migrate/20221005234049_add_index_to_assessment.rb
+++ db/migrate/20221005234049_add_index_to_assessment.rb
@@ -1,6 +1,7 @@
 class AddIndexToAssessment < ActiveRecord::Migration[7.0]
   def change
     add_column :students, :last_canvas_request, :datetime
+    add_column :teachers, :last_canvas_request, :datetime
 
   end
 end
