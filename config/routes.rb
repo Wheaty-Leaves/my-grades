be80ec37b8b2b43365924232fc70e5b942ee3b84
Joshua Burge <joshua.burge@student.adelaide.edu.au>
2022-10-13 10:44:15 +1030

    Teachers can claim courses, and routes are mostly done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   resources :courses do
     resources :enrolments, only: %i[ new index create destroy ] do
-      resources :assessments, only: %i[ new index create destroy ] do
+      resources :assessments, only: %i[ new index create show destroy ] do
         resources :grades, only: %i[ new index create destroy ]
       end
     end
   end
+
   post 'courses/:id', to: 'courses#claim_course'
   get 'my_courses', to: 'courses#my_courses'
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
